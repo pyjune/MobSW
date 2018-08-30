33f9e4c3584ca9d9cc9d296ed44ea2413a260df7
pyjune <pyjune@naver.com>
2018-08-30 15:26:34 +0900

    Create dec2bin.cpp

---
+++ 0830/dec2bin.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void dec2bin(int n);
+
+int main(void)
+{
+	int a = 10;
+
+  dec2bin(a);
+
+
+	printf("\n");
+	system("pause");
+}
+
+
+void dec2bin(int n)
+{
+	for (int i = 31; i >= 0; i--)
+	{
+		printf("%d", (n&(1 << i)) >> i);
+	}
+	printf("\n");
+}
+
+/*
+void dec2bin(int n)
+{
+	for (int i = 31; i >= 0; i--)
+	{
+		if ((a&(1 << i)) == 0)
+			printf("0");
+		else
+			printf("1");
+	}
+}
+*/
