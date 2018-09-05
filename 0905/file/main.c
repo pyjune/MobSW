426d3bb6c49456b641879229a4f51bf0690074f0
pyjune <pyjune@naver.com>
2018-09-05 10:15:35 +0900

    Update main.c

--- 0905/file/main.c
+++ 0905/file/main.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include "function.h"
 
+int g;
+// function.c의 전역변수 gf가 static인 경우 사용불가
+// extern int gf; 
+
 
 int main(void)
 {
 	int a = 10;
 	f1(a);
-
+	f2();
+	printf("%d\n", g);
+	
 	system("pause");
 }
 
