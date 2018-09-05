edd7f9bef7ac745486d4169398c8213a3d302f21
pyjune <pyjune@naver.com>
2018-09-05 10:16:08 +0900

    Update function.c

--- 0905/file/function.c
+++ 0905/file/function.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "main.h"
+//extern int g;
+static int gf = 10; // function.c에서만 사용
+// static을 붙여 function.c에서만 사용..
+// static이 붙은 함수는 .h에 넣지 않음
+static void f2(void);
 
 void f1(int n)
 {
+	f2();
 	printf("%d\n", n);
+	g = 20;
+}
+
+static void f2(void)
+{
+	printf("function.c\n");
 }
