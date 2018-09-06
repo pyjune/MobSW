9cf1a61f0fef1d19c85f494e3d868ecf7f8a0d63
pyjune <pyjune@naver.com>
2018-09-06 15:37:09 +0900

    Update it.c

--- Cortex-M/JOG_EXTI/it.c
+++ Cortex-M/JOG_EXTI/it.c
@@ -191,19 +191,80 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+* @brief This function handles EXTI line0 interrupt.
+*/
+void EXTI0_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI0_IRQn 0 */
+	HAL_Delay(30);
+	if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0 )==GPIO_PIN_SET)
+		jogStatus = J_UP;
+  /* USER CODE END EXTI0_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+  /* USER CODE BEGIN EXTI0_IRQn 1 */
+
+  /* USER CODE END EXTI0_IRQn 1 */
+}
+
+/**
+* @brief This function handles EXTI line1 interrupt.
+*/
+void EXTI1_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI1_IRQn 0 */
+	HAL_Delay(30);
+	if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1 )==GPIO_PIN_SET)
+		jogStatus = J_DOWN;
+  /* USER CODE END EXTI1_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
+  /* USER CODE BEGIN EXTI1_IRQn 1 */
+
+  /* USER CODE END EXTI1_IRQn 1 */
+}
+
+/**
 * @brief This function handles EXTI line2 interrupt.
 */
 void EXTI2_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI2_IRQn 0 */
-HAL_Delay(30); // EXTI2의 인터럽트 우선순위가 systick보다 낮아야 함..
-if(HAL_GPIO_ReadPin(GPIOB, GPIP_PIN_2 )==GPIO_PIN_SET)
-	jogStatus = J_CENTER;
+	HAL_Delay(30);
+	if(HAL_GPIO_ReadPin(J_CENTER_GPIO_Port, J_CENTER_Pin)==GPIO_PIN_SET)
+		jogStatus = J_CENTER;
   /* USER CODE END EXTI2_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
   /* USER CODE BEGIN EXTI2_IRQn 1 */
 
   /* USER CODE END EXTI2_IRQn 1 */
+}
+
+/**
+* @brief This function handles EXTI line[15:10] interrupts.
+*/
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+	// EXTI12
+	if(__HAL_GPIO_EXTI_GET_IT (  GPIO_PIN_12 ))
+	{
+		HAL_Delay(30);
+		if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12)==GPIO_PIN_SET)
+			jogStatus = J_LEFT;
+	}
+	// EXTI13
+	if(__HAL_GPIO_EXTI_GET_IT (  GPIO_PIN_13 ))
+	{
+		HAL_Delay(30);
+		if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13)==GPIO_PIN_SET)
+			jogStatus = J_RIGHT;
+	}
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_12);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
